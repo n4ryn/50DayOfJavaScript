e86b93398164fb9c8f3f3dd9fc58a3b0e48da523
Vinay Kumar <104409366+n4ryn@users.noreply.github.com>
2023-05-29 00:54:37 +0530

    Function which returns a reversed string

---
+++ Day02.js
@@ -0,0 +1,11 @@
+// Create a function which returns a reversed string
+
+const str = "JavaScript is awesome";
+
+const reverseAString = (str) => {
+  // write code here
+  const reversedString = str.split(" ").reverse().join(" ");
+  return reversedString;
+};
+
+console.log(`Reversed string of ${str} is ${reverseAString(str)}`);
